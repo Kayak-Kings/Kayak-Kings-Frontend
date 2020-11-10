43d0e27742b38767cf0d512747ae65f6d66d217b
Julia Collins <jlilianacollins@gmail.com>
2020-11-10 12:08:13 -0800

    added placeholder images

--- src/Components/About.js
+++ src/Components/About.js
@@ -5,12 +5,15 @@ class About extends Component{
     return(
       <div>
         <h1> What We Do</h1>
-        <p> 
-        Mauris non auctor mi, et accumsan velit. Aliquam in lacinia libero. Sed malesuada nec lectus et dapibus. Mauris vel magna et libero accumsan facilisis gravida quis sapien. Maecenas rhoncus est quis ultrices consectetur. Ut vitae ipsum at urna gravida aliquet vel eu mauris. Maecenas nec ullamcorper dui, sit amet feugiat lectus. Quisque laoreet mollis tellus, sed congue elit. Vivamus vel turpis vitae metus fermentum dignissim nec et sem. Interdum et malesuada fames ac ante ipsum primis in faucibus. Donec vel aliquam massa. Sed ac cursus ligula. Sed sit amet nunc tortor.
-        </p>
-        <p>
-        Sed nec commodo nisl. Integer tincidunt convallis elit, in luctus nibh blandit sed. Sed facilisis, nibh at bibendum maximus, sem lacus fermentum dui, sed feugiat magna leo nec arcu. Nullam porttitor, ipsum nec placerat lacinia, ligula dolor porta quam, et malesuada nibh leo vel lorem. Nullam vitae gravida quam, a placerat lorem. Aliquam faucibus dapibus viverra. Nunc faucibus massa nibh, vitae imperdiet est elementum quis. Sed elementum commodo lacinia. Phasellus sed elementum nulla. Phasellus commodo eu dui quis dapibus. Sed varius mauris arcu, vitae viverra ante venenatis nec. Vestibulum ut est id arcu rutrum ultricies. Etiam consectetur congue tellus, quis sodales ligula pretium sed. Integer erat turpis, consectetur et purus dictum, aliquet iaculis eros. Phasellus sit amet pharetra neque. Sed id magna velit.
-        </p>
+        <div class = 'about'>
+          <p> 
+          Mauris non auctor mi, et accumsan velit. Aliquam in lacinia libero. Sed malesuada nec lectus et dapibus. Mauris vel magna et libero accumsan facilisis gravida quis sapien. Maecenas rhoncus est quis ultrices consectetur. Ut vitae ipsum at urna gravida aliquet vel eu mauris. Maecenas nec ullamcorper dui, sit amet feugiat lectus. Quisque laoreet mollis tellus, sed congue elit. Vivamus vel turpis vitae metus fermentum dignissim nec et sem. Interdum et malesuada fames ac ante ipsum primis in faucibus. Donec vel aliquam massa. Sed ac cursus ligula. Sed sit amet nunc tortor.
+          </p>
+          <p>
+          Sed nec commodo nisl. Integer tincidunt convallis elit, in luctus nibh blandit sed. Sed facilisis, nibh at bibendum maximus, sem lacus fermentum dui, sed feugiat magna leo nec arcu. Nullam porttitor, ipsum nec placerat lacinia, ligula dolor porta quam, et malesuada nibh leo vel lorem. Nullam vitae gravida quam, a placerat lorem. Aliquam faucibus dapibus viverra. Nunc faucibus massa nibh, vitae imperdiet est elementum quis. Sed elementum commodo lacinia. Phasellus sed elementum nulla. Phasellus commodo eu dui quis dapibus. Sed varius mauris arcu, vitae viverra ante venenatis nec. Vestibulum ut est id arcu rutrum ultricies. Etiam consectetur congue tellus, quis sodales ligula pretium sed. Integer erat turpis, consectetur et purus dictum, aliquet iaculis eros. Phasellus sit amet pharetra neque. Sed id magna velit.
+          </p>
+        </div>
+        
       </div>
     )
   }
--- src/Components/Routes.js
+++ src/Components/Routes.js
@@ -16,7 +16,7 @@ class Routes extends Component{
       <div>
         <CardGroup>
           <Card>
-            <CardImg top width="100%" src="../../public/logo192.png" alt="Card image cap" />
+            <CardImg top width="150%" src='https://via.placeholder.com/150' alt="Card image cap" />
             <CardBody>
               <CardTitle>Card title</CardTitle>
               <CardSubtitle>Card subtitle</CardSubtitle>
@@ -25,7 +25,7 @@ class Routes extends Component{
             </CardBody>
           </Card>
           <Card>
-            <CardImg top width="100%" src="/assets/318x180.svg" alt="Card image cap" />
+            <CardImg top width="150%" src='https://via.placeholder.com/150' alt="Card image cap" />
             <CardBody>
               <CardTitle>Card title</CardTitle>
               <CardSubtitle>Card subtitle</CardSubtitle>
@@ -34,7 +34,7 @@ class Routes extends Component{
             </CardBody>
           </Card>
           <Card>
-            <CardImg top width="100%" src="/assets/318x180.svg" alt="Card image cap" />
+            <CardImg top width="150%" src='https://via.placeholder.com/150' alt="Card image cap" />
             <CardBody>
               <CardTitle>Card title</CardTitle>
               <CardSubtitle>Card subtitle</CardSubtitle>
